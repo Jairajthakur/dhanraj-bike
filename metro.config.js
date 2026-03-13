86ec1e9837f7c292621a1c5b1363515c84f58954
Jairaj Singh <118998121+Jairajthakur@users.noreply.github.com>
2026-03-13 15:59:54 +0530

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-// metro.config.js
-const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = config;
